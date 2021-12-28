a7b289c24f9ad7d3d43a040eaf66798bf99794e6
DenyaOver <dena_holmes@ukr.net>
2021-12-28 14:52:28 +0100

    53

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,21 +1,27 @@
 import s from './Users.module.css';
 import * as axios from 'axios'
 import userPhoto from '../../../src/assets/images/unnamed.jpg'
+import React from 'react';
 
-let Users = (props) => {
-    if (props.users.length === 0){
+class Users extends React.Component {   
+    
+    constructor (props){
+        super(props);
         axios.get("https://social-network.samuraijs.com/api/1.0/users")
             .then(response => {
-                props.setUsers(response.data.items)
-        })}
-    return(
-        props.users.map(u => <div key = {u.id} className={s.main}>
+            this.props.setUsers(response.data.items)
+            });
+    }
+
+    render() {
+        return (
+        this.props.users.map(u => <div key = {u.id} className={s.main}>
                 <span> 
                     <div className={s}>
                         <img src={u.photos.small != null ? u.photos.small : userPhoto} alt= "icon" />
                     </div>
                     <div>
-                        {u.followed? <button onClick={()=> {props.unfollow(u.id)}} >Unfollow</button> : <button onClick={()=> {props.follow(u.id)}} >Follow</button>}
+                        {u.followed? <button onClick={()=> {this.props.unfollow(u.id)}} >Unfollow</button> : <button onClick={()=> {this.props.follow(u.id)}} >Follow</button>}
                     </div>
                 </span>
                 <span className={s.title}>
@@ -29,7 +35,8 @@ let Users = (props) => {
                     </span>
                 </span>
             </div>)
-    )
+        );
+    }
 }
 
 export default Users
